b3f4c4c4c238577075b692ddee968927e097c7fa
ilhamadikusuma31 <ilhamadikusuma31@gmail.com>
2022-06-21 22:50:37 +0700

    callback

--- NodeJS/node-examples/index.js
+++ NodeJS/node-examples/index.js
@@ -1,15 +1,19 @@
-var rect = require('./rectangle')
+const rect = require('./rectangle');
 
 
 function solveRectangle(l, b) {
     console.log("Solving for rectangle with l = " + l + " and b = " + b);
 
-    if (l <= 0 || b <= 0) {
-        console.log("dimensi harus lebih besar dari 0")
-    } else {
-        console.log("area of a rectangle is " + rect.area(l, b));
-        console.log("perimeter of a rectangle is " + rect.perimeter(l, b));
-    }
+    rect(l, b, (err, kotak) => {
+        if (err) {
+            console.log("Error: ", err.message);
+        } else {
+            console.log("Luas persegi :", kotak.area()) //refrence dari rectangle.js
+            console.log("keliling persegi :", kotak.perimeter())
+        }
+    });
+
+    console.log("program selesai");
 }
 
 solveRectangle(2, 3);
--- NodeJS/node-examples/rectangle.js
+++ NodeJS/node-examples/rectangle.js
@@ -1,2 +1,17 @@
-exports.perimeter = (x, y) => 2 * (x + y);
-exports.area = (x, y) => (x * y);
+module.exports = (x, y, kolbek) => {
+    if (x <= 0 || y <= 0) {
+        setTimeout(() =>
+            // kolbek(error,null)
+            kolbek(new Error("dimensi harus lebih besar dari 0"), null),
+            2000);
+    } else {
+        setTimeout(() =>
+            // kolbek(null, rumus)
+            kolbek(null, {
+                perimeter: () => 2 * (x + y),
+                area: () => (x * y)
+            }),
+            2000);
+
+    }
+}
