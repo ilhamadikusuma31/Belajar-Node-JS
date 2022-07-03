77e4278a3c39d4ad3d4dc92b08f8ba0d64fa26dd
ilhamadikusuma31 <ilhamadikusuma31@gmail.com>
2022-07-04 00:48:29 +0700

    ejs looping array

---
+++ Tutorial-WPU/15.Express-View-Engine/app.js
@@ -0,0 +1,123 @@
+const { render } = require('ejs');
+const express = require('express')
+const app = express()
+const port = 3000
+
+
+app.set('view engine', 'ejs' );
+
+
+
+const mahasiswa = [
+    {
+        nama: 'ilhamadi',
+        prodi:'if',
+    },
+    {
+        nama: 'budi',
+        prodi:'ti',
+    },
+    {
+        nama: 'hamba tuhan',
+        prodi:'si',
+    }
+]
+app.get('/', (req, res) => {
+    //karena pake ejs tinggal panggil nama file didalam views
+    res.render('index',{
+        judul:'halaman home',
+        mahasiswa:mahasiswa
+    });
+    
+});
+
+app.get('/tentang', (req, res) => {
+    res.render('about');
+});
+
+app.get('/kontak', (req, res) => {
+    res.render('contacts');
+});
+
+
+app.get('/produk/:id_nya', (req, res) => {
+    res.send('ID Produk: ' + req.params.id_nya);
+})
+
+app.use('/', (req, res) => {
+    //res.render('index');
+    res.send(`ga ada yang cocok boskuh link yang boskuh masukin: ${req.url}`);
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const http = require('http');
+// const fs = require('fs');
+// const port = 3000;
+
+// const server = http.createServer((req, res) => {
+//     res.writeHead(200, {
+//         'Content-Type': 'text/html',
+//     });
+//     let url = req.url;
+//     console.log(req);
+//     switch (url) {
+//         case '/about':
+//             res.write('<h1>ini adalah halaman about</h1>');
+
+//             break;
+//         case '/home':
+//             res.write('<h1>ini adalah halaman home</h1>');
+//             break;
+
+
+//             // route default-nya
+//         default:
+//             // res.write('<h1>ini adalah halaman default</h1>');
+//             fs.readFile('./index.html', (e, penampung) => { //ini kayak file handling
+//                 if (e) {
+//                     res.writeHead(404);
+//                     res.write('error boskuh');
+//                 } else {
+//                     res.write(penampung); //nampilin ke web
+//                 }
+
+//                 res.end();
+//             });
+//             break;
+
+//     }
+
+
+//     // res.write('<h1>heloo</h1>');
+//     // res.end();
+// });
+
+// server.listen(port, () => {
+//     console.log(`server berjalan di port ${port}`);
+// });
